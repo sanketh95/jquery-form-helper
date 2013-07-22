0e4da15b1fca92aba6d456defd4170de7da06ea9
Sanketh Mopuru <sanketh.mopuru@gmail.com>
2013-07-22 13:39:16 +0530

    Bug in jquery-form-helper.js solved

--- jquery-form-helper.js
+++ jquery-form-helper.js
@@ -9,64 +9,25 @@ var form_helpers = {
 
 
 	add_helper: function(hname,fn){
-		if(!form_helpers.custom_helpers[hname]){
-			form_helpers.custom_helpers[hname] = fn;
+		if(!form_helpers[hname]){
+			form_helpers[hname] = fn;
 		}
 	}
 }
 
-form_helpers.custom_helpers = {}
 
 //mobile localizations
 form_helpers.mob_localization = {
 
-	mob_ind: function(inpel){
-
-		//source : http://stackoverflow.com/questions/995183/how-to-allow-only-numeric-0-9-in-html-inputbox-using-jquery
-		//allows only 10 numbers according to indian mobile standard
-		//mobile helper for indian mobile numbers
-		inpel.keydown(function(event){
-
-			
-
-
-			 // Allow: backspace, delete, tab, escape, and enter
-        if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || event.keyCode == 13 || 
-             // Allow: Ctrl+A
-            (event.keyCode == 65 && event.ctrlKey === true) || 
-             // Allow: home, end, left, right
-            (event.keyCode >= 35 && event.keyCode <= 39)) {
-                 // let it happen, don't do anything
-                 return;
-        }
-        else {
-            // Ensure that it is a number and stop the keypress
-            if (event.shiftKey || (event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
-
-                event.preventDefault(); 
-
-            }   
-            else{
-            	if($(this).val().length >= 10){
-            		event.preventDefault();
-            	}
-            }
-        }
-		});
-	},	
-
-	//default mobile helper
-	//set to india
-	//to change the default replace this.mob_ind(inpel) with other mobile helpers
-	mob: function(inpel){
-		this.mob_ind(inpel);
+	add: function(fname,fn){
+		if(!form_helpers.mob_localization[fname]){
+			form_helpers.mob_localization[fname] = fn;
+		}
 	}
 
 }
 
 //form helpers end here
-
-
 //validate field details
 //validate 
 
@@ -79,28 +40,31 @@ var form_validators  = {
 	//returns true if all required inputs are set
 	//returns false if atleast one input is invalid
 	validate: function(formel){
-		var flag = true;
-		$('[req]',formel).each(function(index,obj){
-			var tg = obj.tagName;
+		formel.submit(function(){
+			var flag = true;
+			$('[req]',formel).each(function(index,obj){
+				var tg = obj.tagName;
+				//If it is input tag
+				//check if the value is valid
+				if(tg == "INPUT"){
 
-			//If it is input tag
-			//check if the value is valid
-			if(tg == "INPUT"){
-				if(obj.value == ""){
-					flag = false;	
-				}
+					if(obj.value == ""){
+						flag = false;	
+					}
 				
-			}
-		});
+				}
+			});
 		
-		return flag;
+			return flag;		
+		});
+
 	},
 
 
 	//used to add a custom validator `validate_fn` to the form `formel`
 	custom_validate: function(formel,validate_fn){
 		formel.submit(function(){
-			return validate_fn();
+			return validate_fn(formel);
 		});
 	},
 
@@ -127,18 +91,13 @@ form_validators.field_validators = {
 
 }
 
-//mobile regexes
-//for different countries
-form_validators.field_validators.mobile_validator = {
-	//mobile validation for indian mobile numbers
-	mob_ind: function(){
-
-	}
-}
 
 //called when there any input field value is invalid
 function error(inpel){
 
+}
+
+function inputwarn(inpel){
 }
 
 
