8ac06049d0c0cb8b1279b80b75a9735c28c561e2
Sanketh Mopuru <sanketh.nuclear@gmail.com>
2013-08-03 10:35:46 +0530

    master.js added

--- master.js
+++ master.js
@@ -104,5 +104,74 @@ function inputwarn(inpel){
 	
 }
 
+//form helper code for indian mobiles
+form_helpers.mobile_localization.add('mob_ind',function(inpel){
+	inpel.keydown(function(event){
 
+			
+
+
+			 // Allow: backspace, delete, tab, escape, and enter
+        if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || event.keyCode == 13 || 
+             // Allow: Ctrl+A
+            (event.keyCode == 65 && event.ctrlKey === true) || 
+             // Allow: home, end, left, right
+            (event.keyCode >= 35 && event.keyCode <= 39)) {
+                 // let it happen, don't do anything
+                 return;
+        }
+        else {
+            // Ensure that it is a number and stop the keypress
+            if (event.shiftKey || (event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+
+                event.preventDefault(); 
+
+            }   
+            else{
+            	if($(this).val().length >= 10){
+            		event.preventDefault();
+            	}
+            }
+        }
+		});
+});
+
+//date field validator
+form_validators.add_custom_field_validator('date',function(d,format,separator){
+	if(!isNaN(separator)){
+		return false;
+	}
+	datearr = d.split(separator);
+	var month = datearr[format.indexOf('m')];
+	var day = datearr[format.indexOf('d')];
+	var year = datearr[format.indexOf('y')];
+
+	if(month < 0 || month > 12){
+	return false;
+	}
+	if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12 ){
+		if(day < 0 || day > 31){
+			return false;
+		}
+	}
+	else if(month == 2){
+		if(day < 0 || day > 28){
+			return false;
+		}
+	}
+	else{
+		if(day < 0 || day > 30){
+			return false;
+		}
+	}
+	return true;
+
+});
+
+
+//email validator
+form_validators.add_custom_field_validator('email',function(email){
+   var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;  
+   return emailPattern.test(email);   
+});
 
